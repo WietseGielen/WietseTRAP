36018d8d9eee3ee6115dfe46105e6e437e926de0
Wietse Gielen <wietse.gielen@gmail.com>
2019-05-08 12:05:11 +0200

    Initial commit

---
+++ JS/WietseTRAP.js
@@ -0,0 +1,8 @@
+function toggleCode() {
+    var x = document.getElementById("toggleCode");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
